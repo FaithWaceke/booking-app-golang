fb59f55e1d4c464d4403b8fd328c980529724e15
FaithWaceke <wacekenjosh@gmail.com>
2023-09-30 13:38:23 +0300

    commit added

--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	var confrenceName = "Go confrence"
+	const confrenceTicket = 50
+	var remainingTicket = 50
+
+	fmt.Printf("Welcome to %v booking application\n", confrenceName)
+	fmt.Printf("We have a total of %v ticket and %v are still available.\n", confrenceTicket, remainingTicket)
+	fmt.Printf("Get your ticket here to attend\n")
+
+	var userName string
+	var userTicket int
+	// ask user for their name
+
+	userName = "Phebby"
+	userTicket = 2
+	fmt.Printf("user %v booked %v tickets.\n", userName, userTicket)
+
 }
